050fa424557bf87653012a95d192ccf137e473e9
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-07-21 18:55:21 -0600

    Create fetch to obtain data using coordinates and linking them to the weather api

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,40 @@
 $(document).ready(function () {
-    let city;
+    let city = "Salt Lake City"; //Will be used to input a city name
+
     const APIKEY = "ee3bdd85ae12cf0b59312c7a5aa514bb";
-    let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKEY;
-    let badStatus = "Error: ";
-  
-    function getApi() {
-        fetch(queryURL)
-            .then(function (response) {
-                console.log(response);
-                if(response.status === 400) {
-                    console.log(response.status);
-                    $("#error").text(badStatus + response.status);
-                } else {
-                    return response.json();
+
+
+
+
+
+
+    let geoApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKEY;
+
+    fetch(geoApi)
+        .then(function (response) {
+            console.log(response);
+            // if (response.status === 400) {
+            //     console.log(response.status);
+            //     $("#error").text(badStatus + response.status);
+            // } else {
+            return response.json();
+        })
+        .then(function (coord) {
+            for (let i = 0; i < coord.length; i++) {
+                let lat = coord[i].lat;
+                let lon = coord[i].lon;
+                console.log(lat);
+                console.log(lon);
+                async function getWeatherApi() {
+                    let weatherApi = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY;
+                    const response = await fetch(weatherApi);
+                    const data = await response.json();
+                    console.log(data);
                 }
-            })
-            .then(function (data) {
-                console.log(data);
-            });
-    }
-
-    getApi();
-
-
-
-
-
-});
-
+                getWeatherApi().catch(error => {
+                    console.log("error");
+                    console.error(error);
+                });
+            }
+        });
+})
