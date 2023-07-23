7c3a5e727e619da4fb6bac7865e76326216ee1e7
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-07-23 17:11:17 -0600

    Add weather data into weather cards.

--- README.md
+++ README.md
@@ -40,7 +40,6 @@ Useful links:
 * Tutorials on how to use the Bulma CSS library- https://www.youtube.com/@NetNinja
 * CSS Gradient for my colors: https://cssgradient.io/
 
-
 ## License
 
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,22 @@
 $(document).ready(function () {
 
-
+    console.log(dayjs().add(1, "day").format("YYYY-MM-DD 12:00:00"));
     let currentCityWeather = document.getElementById("city-name-and-date");
     let city = document.querySelector("#city");
 
 
     const APIKEY = "ee3bdd85ae12cf0b59312c7a5aa514bb"; //This key will allow us to "fetch" the data from the weather api
 
-    //Will be used to input a city name
+    //Will be used to input a city name and run our function to get coordinates
     let submit = $("#submit");
     submit.on("click", function () {
         api.getLocation();
-        //currentCityWeather.innerText = data.city.name;
     });
 
+
+    //Created an object of functions
     const api = {
+        //Grabs the coordinates of the city entered
         getLocation: () => {
             let geoApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + city.value + "&appid=" + APIKEY;
 
@@ -29,6 +31,7 @@ $(document).ready(function () {
                     currentCityWeather.textContent = coord[0].name;
                     let latitude = coord[0].lat;
                     let longitude = coord[0].lon;
+                    //We can concatenate variables into our url and use them as parameters
                     let weatherApi = "https://api.openweathermap.org/data/2.5/forecast?current&lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + APIKEY;
                     fetch(weatherApi)
                         .then(function (resp) {
@@ -36,16 +39,134 @@ $(document).ready(function () {
                         })
                         .then(function (data) {
                             console.log(data);
-                            api.displayWeather(data);
+                            api.displayWeather(data); //<--Will run the function below and passes the data that was fetched here to that function
                         })
                 })
-
-
         },
+        //This runs through all the data and "handpicks" what we want to then add it to our html elements
         displayWeather: (resp) => {
+            console.log(resp);
+            for (let i = 0; i < resp.list.length; i++) {
+                let unix = resp.list[i].dt; //Grabs the list of unix timestamps
+                let stringUnix = unix.toString(); //converts the unix timestamp into a string
+                let date = dayjs.unix(stringUnix).format("MMMM DD YYYY"); //formats the converted unix timestamp date
+                let weatherDate;
+                let iconImage;
+                let iconURL = "https://openweathermap.org/img/wn/";
+                let imageAlt;
+                let weatherDescription;
+                let temp;
+                let windSpeed;
+                let humidity;
 
+                console.log(date);
+
+
+                //Adds our dates to our  weather cards
+                if (dayjs().add(1, "day").format("YYYY-MM-DD 12:00:00") === resp.list[i].dt_txt) {
+                    weatherDate = $("#date-01").html(date);
+                    iconImage = $("#icon-01").attr("src", iconURL + resp.list[i].weather[0].icon + "@4x.png");
+                    imageAlt = $(".img-alt-01").attr("alt", resp.list[i].weather[0].description);
+                    weatherDescription = $("#weather-description-01").html(resp.list[i].weather[0].description.toUpperCase());
+                    temp = $("#current-weather-01").children().eq(0).text("Temp: " + resp.list[i].main.temp + " °F");
+                    windSpeed = $("#current-weather-01").children().eq(1).text("Wind Speed: " + resp.list[i].wind.speed + "mph");
+                    humidity = $("#current-weather-01").children().eq(2).text("Humidity: " + resp.list[i].main.humidity + "%");
+
+                }
+                if (dayjs().add(2, "day").format("YYYY-MM-DD 12:00:00") === resp.list[i].dt_txt) {
+                    weatherDateTwo = $("#date-02").html(date);
+                    iconImage = $("#icon-02").attr("src", iconURL + resp.list[i].weather[0].icon + "@4x.png")
+                    imageAlt = $(".img-alt-02").attr("alt", resp.list[i].weather[0].description);
+                    weatherDescription = $("#weather-description-02").html(resp.list[i].weather[0].description.toUpperCase());
+                    temp = $("#current-weather-02").children().eq(0).text("Temp: " + resp.list[i].main.temp + " °F");
+                    windSpeed = $("#current-weather-02").children().eq(1).text("Wind Speed: " + resp.list[i].wind.speed + "mph");
+                    humidity = $("#current-weather-02").children().eq(2).text("Humidity: " + resp.list[i].main.humidity + "%");
+
+                }
+                if (dayjs().add(3, "day").format("YYYY-MM-DD 12:00:00") === resp.list[i].dt_txt) {
+                    weatherDateOne = $("#date-03").html(date);
+                    iconImage = $("#icon-03").attr("src", iconURL + resp.list[i].weather[0].icon + "@4x.png")
+                    imageAlt = $(".img-alt-03").attr("alt", resp.list[i].weather[0].description);
+                    weatherDescription = $("#weather-description-03").html(resp.list[i].weather[0].description.toUpperCase());
+                    temp = $("#current-weather-03").children().eq(0).text("Temp: " + resp.list[i].main.temp + " °F");
+                    windSpeed = $("#current-weather-03").children().eq(1).text("Wind Speed: " + resp.list[i].wind.speed + "mph");
+                    humidity = $("#current-weather-03").children().eq(2).text("Humidity: " + resp.list[i].main.humidity + "%");
+
+                }
+                if (dayjs().add(4, "day").format("YYYY-MM-DD 12:00:00") === resp.list[i].dt_txt) {
+                    weatherDateTwo = $("#date-04").html(date);
+                    iconImage = $("#icon-04").attr("src", iconURL + resp.list[i].weather[0].icon + "@4x.png")
+                    imageAlt = $(".img-alt-04").attr("alt", resp.list[i].weather[0].description);
+                    weatherDescription = $("#weather-description-04").html(resp.list[i].weather[0].description.toUpperCase());
+                    temp = $("#current-weather-04").children().eq(0).text("Temp: " + resp.list[i].main.temp + " °F");
+                    windSpeed = $("#current-weather-04").children().eq(1).text("Wind Speed: " + resp.list[i].wind.speed + "mph");
+                    humidity = $("#current-weather-04").children().eq(2).text("Humidity: " + resp.list[i].main.humidity + "%");
+
+                }
+                if (dayjs().add(5, "day").format("YYYY-MM-DD 12:00:00") === resp.list[i].dt_txt) {
+                    weatherDateTwo = $("#date-05").html(date);
+                    iconImage = $("#icon-05").attr("src", iconURL + resp.list[i].weather[0].icon + "@4x.png")
+                    imageAlt = $(".img-alt-05").attr("alt", resp.list[i].weather[0].description);
+                    weatherDescription = $("#weather-description-05").html(resp.list[i].weather[0].description.toUpperCase());
+                    temp = $("#current-weather-05").children().eq(0).text("Temp: " + resp.list[i].main.temp + " °F");
+                    windSpeed = $("#current-weather-05").children().eq(1).text("Wind Speed: " + resp.list[i].wind.speed + "mph");
+                    humidity = $("#current-weather-05").children().eq(2).text("Humidity: " + resp.list[i].main.humidity + "%");
+
+                }
+
+
+
+
+
+            }
         }
+        //resp[i].list)
     }
+
+    //
+
+    //let dateOne = resp.list[5].dt;
+
+
+
+
+
+
+    // let weatherData = $("#weather-card");
+    // weatherData.html(resp.list.map((day, index) => {
+    //     if (index === 0) { 
+    //         let date = new Date(day.dt * 1000);
+
+
+
+
+
+
+    //         return `<div class="column is-4 is-desktop">
+    //         <div class="card">
+    //         <header class="card-header">
+    //         <p class="card-header-title has-text-white">${date.toDateString()}</p>
+    //     </header>
+    //     <div class="card-image">
+    //         <figure class="image">
+    //             <img src="https://openweathermap.org/img/wn/" alt="" class="src">
+    //         </figure>
+    //     </div>
+    //     <div class="card-content pb-6 pl-0">
+    //         <div class="content has-text-white">
+    //             <ul>
+    //                 <li>Temp: </li>
+    //                 <li>Wind: </li>
+    //                 <li>Humidity: </li>
+    //             </ul>
+    //         </div>
+    //     </div>
+    // </div>
+    // </div>`
+    //     }
+    // }).join(" "));
+
+
 
 
 
